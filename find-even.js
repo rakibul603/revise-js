77642ff1bf09de4729dacc0287914eaafce5074e
rakib <rakibco66@gmail.com>
2026-02-07 22:05:09 +0600

    find even and odd number

---
+++ find-even.js
@@ -0,0 +1,17 @@
+
+for (let i = 0; i < 10 ; i = i + 2) {
+    // console.log(i)
+    if(i % 2 === 0){
+        console.log(i)
+    }
+}
+
+
+for (let i = 0; i < 10 ; i++) {
+    // console.log(i)
+    if(i % 2 === 1){
+        console.log(i)
+    }
+}
+
+console.log('ame')
