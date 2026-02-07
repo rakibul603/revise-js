bfb1b87f26af5e07e3b19a48177806be39da52de
rakib <rakibco66@gmail.com>
2026-02-07 20:53:26 +0600

    condition

--- condition.js
+++ condition.js
@@ -1,12 +1,29 @@
-let  result = 4.0;
+// let  result = 4.0;
 
-if(result === 5.0) {
-    console.log('I buy computer')
-}
-else if (result >= 4.0) {
-    console.log('laptop ')
+// if(result === 5.0) {
+//     console.log('I buy computer')
+// }
+// else if (result >= 4.0) {
+//     console.log('laptop ')
+// }
+
+// else{
+//     console.log('muri kha')
+// }
+
+let balance = 2000;
+
+let withdrawAmount = 1500;
+
+if(withdrawAmount <= balance) {
+    if(withdrawAmount % 500 === 0 ) {
+        console.log('taka nyw moja masti koro')
+    }
+    else{
+        console.log('thikray balnce day')
+    }
 }
 
 else{
-    console.log('muri kha')
+    console.log('duray jaia mor')
 }
