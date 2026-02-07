445f8af4f228ab1fbd07c695183bc780747be195
rakib <rakibco66@gmail.com>
2026-02-07 20:30:28 +0600

    operatior

---
+++ oparator.js
@@ -0,0 +1,11 @@
+
+
+let bangla = 130;
+let eniglis = 60;
+
+
+console.log(bangla + eniglis);
+console.log(bangla - eniglis);
+console.log(bangla * eniglis);
+console.log(bangla % eniglis);
+console.log((bangla / eniglis).toFixed(3))
