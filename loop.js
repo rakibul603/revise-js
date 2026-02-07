3465e880ed17494c506353ec2e909cdb0880055f
rakib <rakibco66@gmail.com>
2026-02-07 21:52:42 +0600

    loops

---
+++ loop.js
@@ -0,0 +1,39 @@
+// // for loop 
+
+// for (let i = 0; i < 40; i ++) {
+//     console.log("love you")
+// }
+
+
+
+// let i  = 0;
+
+// while(i < 50) {
+//     i++
+//     console.log('i Love ')
+// }
+
+
+
+
+
+// let i = 0;
+
+// do {
+
+//     console.log('i Love ')
+//     i++
+// }
+
+// while (i < 10) {
+
+// }
+
+
+for (let i = 0; i < 30; i ++) {
+    if(i === 10) {
+        console.log("break", i)
+        continue;
+    }
+    console.log(i)
+}
